f6c41d2b011f28bbd6907bf6a79d367f7a738474
ReneP8 <Rene-Baving@web.de>
2022-12-02 16:27:45 +0100

    finished part two of day 2

--- day2/scoreStrategie.py
+++ day2/scoreStrategie.py
@@ -9,43 +9,94 @@ file = open("input.txt", "r")
 # reading lines from text file
 lines = file.readlines()
 
-totalScore = 0
-for line in lines:
-    figure_score = 0
-    outcome_score = 0
-    line = line.strip("\n").split()
 
-    opponent = line[0]
-    me = line[1]
-
-    if me == "X":
-        figure_score = 1
-    elif me == "Y":
-        figure_score = 2
-    elif me == "Z":
-        figure_score = 3
-
-    if opponent == "A" and me == "X":
-        outcome_score = 3
-    elif opponent == "A" and me == "Y":
-        outcome_score = 6
-    elif opponent == "A" and me == "Z":
+def part_one():
+    totalScore = 0
+    for line in lines:
+        figure_score = 0
         outcome_score = 0
+        line = line.strip("\n").split()
 
-    if opponent == "B" and me == "X":
+        opponent = line[0]
+        me = line[1]
+
+        if me == "X":
+            figure_score = 1
+        elif me == "Y":
+            figure_score = 2
+        elif me == "Z":
+            figure_score = 3
+
+        if opponent == "A" and me == "X":
+            outcome_score = 3
+        elif opponent == "A" and me == "Y":
+            outcome_score = 6
+        elif opponent == "A" and me == "Z":
+            outcome_score = 0
+
+        if opponent == "B" and me == "X":
+            outcome_score = 0
+        elif opponent == "B" and me == "Y":
+            outcome_score = 3
+        elif opponent == "B" and me == "Z":
+            outcome_score = 6
+
+        if opponent == "C" and me == "X":
+            outcome_score = 6
+        elif opponent == "C" and me == "Y":
+            outcome_score = 0
+        elif opponent == "C" and me == "Z":
+            outcome_score = 3
+
+        totalScore += figure_score + outcome_score
+
+    print("Your total score would be: " + str(totalScore))
+
+
+def part_two():
+    totalScore = 0
+    for line in lines:
+        figure_score = 0
         outcome_score = 0
-    elif opponent == "B" and me == "Y":
-        outcome_score = 3
-    elif opponent == "B" and me == "Z":
-        outcome_score = 6
+        line = line.strip("\n").split()
 
-    if opponent == "C" and me == "X":
-        outcome_score = 6
-    elif opponent == "C" and me == "Y":
-        outcome_score = 0
-    elif opponent == "C" and me == "Z":
-        outcome_score = 3
+        opponent = line[0]
+        me = line[1]
 
-    totalScore += figure_score + outcome_score
+        if opponent == "A" and me == "X":
+            figure_score = 3
+            outcome_score = 0
+        elif opponent == "A" and me == "Y":
+            figure_score = 1
+            outcome_score = 3
+        elif opponent == "A" and me == "Z":
+            figure_score = 2
+            outcome_score = 6
 
-print("Your total score would be: " + str(totalScore))
+        if opponent == "B" and me == "X":
+            figure_score = 1
+            outcome_score = 0
+        elif opponent == "B" and me == "Y":
+            figure_score = 2
+            outcome_score = 3
+        elif opponent == "B" and me == "Z":
+            figure_score = 3
+            outcome_score = 6
+
+        if opponent == "C" and me == "X":
+            figure_score = 2
+            outcome_score = 0
+        elif opponent == "C" and me == "Y":
+            figure_score = 3
+            outcome_score = 3
+        elif opponent == "C" and me == "Z":
+            figure_score = 1
+            outcome_score = 6
+
+        totalScore += figure_score + outcome_score
+
+    print("2. Your total score would be: " + str(totalScore))
+
+
+part_one()
+part_two()
