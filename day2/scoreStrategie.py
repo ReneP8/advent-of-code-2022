382d3a338782fd03d13141ee65558d053186dd60
ReneP8 <Rene-Baving@web.de>
2022-12-02 16:14:52 +0100

    finished part 1 of day 2

---
+++ day2/scoreStrategie.py
@@ -0,0 +1,51 @@
+# read line by line
+# get value for choosen piece (rock=1, paper=2, scissors=3)
+# get value for outcome
+# sum up both values
+# calculate total sum by adding sum
+
+file = open("input.txt", "r")
+
+# reading lines from text file
+lines = file.readlines()
+
+totalScore = 0
+for line in lines:
+    figure_score = 0
+    outcome_score = 0
+    line = line.strip("\n").split()
+
+    opponent = line[0]
+    me = line[1]
+
+    if me == "X":
+        figure_score = 1
+    elif me == "Y":
+        figure_score = 2
+    elif me == "Z":
+        figure_score = 3
+
+    if opponent == "A" and me == "X":
+        outcome_score = 3
+    elif opponent == "A" and me == "Y":
+        outcome_score = 6
+    elif opponent == "A" and me == "Z":
+        outcome_score = 0
+
+    if opponent == "B" and me == "X":
+        outcome_score = 0
+    elif opponent == "B" and me == "Y":
+        outcome_score = 3
+    elif opponent == "B" and me == "Z":
+        outcome_score = 6
+
+    if opponent == "C" and me == "X":
+        outcome_score = 6
+    elif opponent == "C" and me == "Y":
+        outcome_score = 0
+    elif opponent == "C" and me == "Z":
+        outcome_score = 3
+
+    totalScore += figure_score + outcome_score
+
+print("Your total score would be: " + str(totalScore))
