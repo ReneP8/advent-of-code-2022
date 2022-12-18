345329173e42597182e3475e2563c48c01d307e1
ReneP8 <Rene-Baving@web.de>
2022-12-18 20:34:17 +0100

    finished challenge day 4 part 2

--- day4/main.py
+++ day4/main.py
@@ -20,3 +20,20 @@ def ranges_fully_contained():
             fully_contained += 1
 
     print(fully_contained)
+
+
+def ranges_overlap():
+    overlapping = 0
+
+    for line in lines:
+        sections = line.strip("\n").split(',')
+
+        range1 = range(int(sections[0].split('-')[0]),
+                       int(sections[0].split('-')[1])+1)
+        range2 = range(int(sections[1].split('-')[0]),
+                       int(sections[1].split('-')[1])+1)
+
+        if (range1.start <= range2.stop-1) and (range1.stop-1 >= range2.start):
+            overlapping += 1
+
+    print(overlapping)
