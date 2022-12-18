d744eee19dcac62101c4f56a661fcebc4eed0a6b
ReneP8 <Rene-Baving@web.de>
2022-12-18 23:13:25 +0100

    day6: finished part 2

--- day6/main.py
+++ day6/main.py
@@ -3,12 +3,29 @@ file = open("input.txt", "r")
 # reading lines from text file
 line = file.readline()
 
-found_index = -1
-for index in range(len(line) - 3):
-    substring = line[index:index+4]
-    # Check if the substring has four unique characters
-    if len(set(substring)) == 4:
-        if found_index == -1:
-            found_index = index+4
 
-print(found_index)
+def part_one():
+    found_index = -1
+    for index in range(len(line) - 3):
+        substring = line[index:index+4]
+        # Check if the substring has four unique characters
+        if len(set(substring)) == 4:
+            if found_index == -1:
+                found_index = index+4
+
+    print(found_index)
+
+
+def part_two():
+    found_index = -1
+    for index in range(len(line) - 13):
+        substring = line[index:index+14]
+        # Check if the substring has four unique characters
+        if len(set(substring)) == 14:
+            if found_index == -1:
+                found_index = index+14
+
+    print(found_index)
+
+
+part_two()
